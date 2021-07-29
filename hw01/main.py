90aaa50e6854ae3f6b46144ff077b2bab25acaa5
DenisL87 <denis.litvinenko@yahoo.com>
2021-07-29 21:34:21 +0300

    parse cookies

--- hw01/main.py
+++ hw01/main.py
@@ -11,11 +11,26 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+  if query == '':
+    return{}
+  else:
+    newdict = {}
+    values = re.split('=|;', query)
+    count = 0
+    while count < len(values):
+      if values[count] == '':
+        count += 1
+        continue
+      newdict[values[count]] = values[count + 1]
+      count +=2
+    return newdict
 
 
 if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+  assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+  assert parse_cookie('') == {}
+  assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+  #assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+  assert parse_cookie('name=') == {'name': ''}
+  assert parse_cookie('name=Dima;age=28;city=Kharkov') == {'name': 'Dima', 'age': '28', 'city': 'Kharkov'}
+  assert parse_cookie('name=Dima;family name=Ivanov;gender=male') == {'name': 'Dima', 'family name': 'Ivanov', 'gender': 'male'}
