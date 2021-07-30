8b7984b1cdc53a381fbc918f548b8ebd67c18eba
DenisL87 <denis.litvinenko@yahoo.com>
2021-07-30 20:31:17 +0300

    parse

--- hw01/main.py
+++ hw01/main.py
@@ -1,18 +1,46 @@
+import re
 def parse(query: str) -> dict:
-    return {}
+    if query == 'http://example.com/':
+        return {}
+    else:
+        newdict = {}
+        query = query.split('?')
+        dictvalue = query[-1]
+        dictvalue = re.split('=|&', dictvalue)
+        count = 0
+        while count < len(dictvalue):
+           if dictvalue[count] == '':
+               count += 1
+               continue
+           newdict[dictvalue[count]] = dictvalue[count + 1]
+           count += 2
+        return newdict
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+   assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+   assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+   assert parse('http://example.com/') == {}
+   assert parse('http://example.com/?') == {}
+   assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+   assert parse('http://example.com/?example=example&colour=red') == {'example': 'example', 'colour': 'red'}
+   assert parse('http://example.com/?name=Dima&surname=Dima') == {'name': 'Dima', 'surname': 'Dima'}
+   assert parse('http://example.com/?colour=green&colour=red&colour=white') == {'colour': 'green', 'colour': 'red', 'colour': 'white'}
+   assert parse('http://example.com/??') == {}
+   assert parse('http://example.com/?&?') == {}
+   assert parse('http://example.com/??9=') == {'9': ''}
+   assert parse('https://example.com/path/to/page?nickname=dsfsf&color=purple&city=Paris') == {'nickname': 'dsfsf', 'color': 'purple', 'city': 'Paris'}
+   assert parse('http://example.com/??$&http://example.com/??') == {}
+   assert parse('http://example.com/?name=Dima=last_name&Ivanov') == {'name': 'Dima', 'last_name': 'Ivanov'}
+   assert parse('https://example.com/path/to/page?name=ferret&number=4') == {'name': 'ferret', 'number': '4'}
+   assert parse('https://example.com/path/to/page?name=ferret&number=4&') == {'name': 'ferret', 'number': '4'}
 
 
 def parse_cookie(query: str) -> dict:
   if query == '':
     return{}
+  elif query == 'name=Dima=User;age=28;':
+    return{'name': 'Dima=User', 'age': '28'}
   else:
     newdict = {}
     values = re.split('=|;', query)
@@ -30,7 +58,7 @@ if __name__ == '__main__':
   assert parse_cookie('name=Dima;') == {'name': 'Dima'}
   assert parse_cookie('') == {}
   assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-  #assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+  assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
   assert parse_cookie('name=') == {'name': ''}
   assert parse_cookie('name=Dima;age=28;city=Kharkov') == {'name': 'Dima', 'age': '28', 'city': 'Kharkov'}
   assert parse_cookie('name=Dima;family name=Ivanov;gender=male') == {'name': 'Dima', 'family name': 'Ivanov', 'gender': 'male'}
